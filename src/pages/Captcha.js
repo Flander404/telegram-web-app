f1f6d65273252e0ee18dc50eaf2fe55ab7ca71ac
Flander404 <109587664+Flander404@users.noreply.github.com>
2024-01-19 11:19:04 +0500

    test

--- src/pages/Captcha.js
+++ src/pages/Captcha.js
@@ -7,14 +7,14 @@ const Captcha = () => {
   return (
     <div className="captcha">
       <h1>Потвердите пожалуйста что вы не робот</h1>
+      <img src={tg.initDataUnsafe?.user.photo_url} alt="" />
       <button>Click test</button>
       <span>
         {tg.initDataUnsafe?.user.first_name}
       </span>
       <span>
-        {tg.initDataUnsafe?.user.last_name}
+        {tg.initDataUnsafe?.user.username}
       </span>
-      <img src={tg.initDataUnsafe?.user.photo_url} alt="" />
     </div>
   );
 };
