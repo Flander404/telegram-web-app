cde6847a3bfd73acc0f6c8f95c62ccd2f9e676b2
Flander404 <109587664+Flander404@users.noreply.github.com>
2024-01-19 11:04:52 +0500

    test

--- src/pages/Captcha.js
+++ src/pages/Captcha.js
@@ -5,16 +5,6 @@ const tg = window.Telegram.WebApp;
 
 const Captcha = () => {
   const user = tg.initDataUnsafe?.user;
-  const getUserProfilePicture = () => {
-    if (user?.photo) {
-      const photoObj = user.photo.big || user.photo.small || user.photo.medium;
-      if (photoObj) {
-        return photoObj.photo_url;
-      }
-    }
-    // URL запасного изображения или укажите путь к своему изображению-заполнителю
-    return "путь/к/изображению-заполнителю.png";
-  };
   return (
     <div className="captcha">
       <h4>Потвердите пожалуйста что вы не робот</h4>
