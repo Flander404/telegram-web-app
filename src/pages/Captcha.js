98a7f8c59698c738ac7636cdc655daf1cf944888
Flander404 <109587664+Flander404@users.noreply.github.com>
2024-01-19 11:00:14 +0500

    test

--- src/pages/Captcha.js
+++ src/pages/Captcha.js
@@ -1,16 +1,26 @@
 import React from "react";
 import "friendly-challenge/widget";
 
-const tg = window.Telegram.WebApp
+const tg = window.Telegram.WebApp;
 
 const Captcha = () => {
+  const user = tg.initDataUnsafe?.user;
+  const getUserProfilePicture = () => {
+    if (user?.photo) {
+      const photoObj = user.photo.big || user.photo.small || user.photo.medium;
+      if (photoObj) {
+        return photoObj.photo_url;
+      }
+    }
+    // URL запасного изображения или укажите путь к своему изображению-заполнителю
+    return "путь/к/изображению-заполнителю.png";
+  };
   return (
     <div className="captcha">
-      <h1>Потвердите пожалуйста что вы не робот</h1>
+      <h4>Потвердите пожалуйста что вы не робот</h4>
+      <img src={getUserProfilePicture()} alt="Аватар пользователя" />
       <button>Click test</button>
-      <span>
-        {tg.initDataUnsafe?.user?.username}
-      </span>
+      <span>{user}</span>
     </div>
   );
 };
